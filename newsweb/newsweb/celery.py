f9f4ef2da18a155afeefd07988cb9dd1dcc86be6
Sanzhar Spatayev <112723529+wolverkz@users.noreply.github.com>
2023-06-09 15:30:46 +0600

    Update celery.py

--- newsweb/newsweb/celery.py
+++ newsweb/newsweb/celery.py
@@ -1,18 +1,14 @@
-from __future__ import absolute_import, unicode_literals
-
-import os
-
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsweb.settings')
+app = Celery('proj',
+             broker='amqp://',
+             backend='rpc://',
+             include=['proj.tasks'])
 
-app = Celery('newsweb')
+# Optional configuration, see the application user guide.
+app.conf.update(
+    result_expires=3600,
+)
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
-
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+if __name__ == '__main__':
+    app.start()
