5ac115ef408dc8a8462a0f17a255c882c09c1618
wolverkz <s.spatayev@gmail.com>
2023-06-16 11:08:29 +0600

    created my first restAPI

--- newsweb/newsweb/settings.py
+++ newsweb/newsweb/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 's.spatayev@gmail.com'
+EMAIL_HOST_PASSWORD = 'portal2000sp'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 300
