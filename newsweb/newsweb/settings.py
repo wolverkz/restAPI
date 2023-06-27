f832c15cafedad944266a5955197f6187751791c
wolverkz <s.spatayev@gmail.com>
2023-06-27 10:54:19 +0600

    created my first restAPI

--- newsweb/newsweb/settings.py
+++ newsweb/newsweb/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'newsweb',
         'USER': 'postgres',
         'PASSWORD': 'Portal1911sp',
-        'HOST': 'db',
+        'HOST': 'restapi_db_1',
         'PORT': '2345',
     }
 }
