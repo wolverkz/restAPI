fb814595f9a233a70cd38f75214eb47cea535435
wolverkz <s.spatayev@gmail.com>
2023-07-25 17:47:15 +0600

    created my first restAPI

--- newsweb/newsapp/tasks.py
+++ newsweb/newsapp/tasks.py
@@ -1,14 +1,15 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
 
 
 @shared_task()
 def send_newsletter_email():
+    subject = 'New newsletter'
+    message = 'Hey there, new newsletter is out!'
+    from_email = 'newswebtest3@gmail.com'
+    recipient_emails = list(
+        User.objects.values_list('email', flat=True))
 
-    send_mail(
-        'New newsletter created',
-        'Hey there, new newsletter is out!',
-        's.spatayev@gmail.com',
-        ['s.spatayev@gmail.com'],
-        fail_silently=False
-    )
+    for email in recipient_emails:
+        send_mail(subject, message, from_email, [email], fail_silently=False)
