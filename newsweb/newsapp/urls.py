c084dc4ebb3af90561c8f93670dcaa83fbbfd102
wolverkz <s.spatayev@gmail.com>
2023-07-26 17:25:26 +0600

    created my first restAPI

--- newsweb/newsapp/urls.py
+++ newsweb/newsapp/urls.py
@@ -2,12 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-
 router = DefaultRouter()
 router.register(r'newsletters', views.NewsletterViewSet, basename='newsletter')
 router.register(r'articles', views.ArticleViewSet, basename='article')
 router.register(r'users', views.UserViewSet, basename='user')
-router.register(r'')
 
 urlpatterns = [
     path('api/register/', views.user_registration, name='user-registration'),
